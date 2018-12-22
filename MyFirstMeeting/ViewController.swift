6e03e1c9e12c7533485455a869108b78bf7706c0
안병욱 <anbyeong-ug@anbyeong-ug-ui-macbook-pro.local>
2018-12-22 20:57:34 +0900

    Initial Commit

---
+++ MyFirstMeeting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstMeeting
+//
+//  Created by 안병욱 on 07/10/2018.
+//  Copyright © 2018 안병욱. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
